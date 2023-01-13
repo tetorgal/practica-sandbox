15274fe0cfe7483a3f95a5a1e4ec9f23bf68a3b7
tetorgal <105248557+tetorgal@users.noreply.github.com>
2023-01-13 14:38:06 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hola :D</h1>
+<div>
+  <p>Hector Fernando Galarza Carreño</p>
+  <p>Universidad Tecnologica de Durango</p>
+  Este es mi repositorio de github 
+  <a href="https://github.com/tetorgal/contador-react.git" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
